bf3773798efdeca1302718859126bac4e846a927
mrkie <mrkie@DESKTOP-04STFUU>
2024-07-03 18:03:59 +0100

    Static-Repository Commit

---
+++ Contacts.Maui/Models/ContactRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Maui.Models
+{
+    public static class ContactRepository
+    {
+        public static List<Contact> _contacts = new() 
+        {
+            new Contact { ContactId = 0, Name = "John Doe", Email = "JohnDoe@Gmail.com" },
+            new Contact { ContactId = 1, Name = "Jane Doe", Email = "JaneDoe@Gmail.com" },
+            new Contact { ContactId = 2, Name = "Tom Hanks", Email = "TomHanks@Gmail.com" },
+            new Contact { ContactId = 3, Name = "Frank Liu", Email = "FrankLiu@Gmail.com" }
+        };
+
+        public static List<Contact> GetContacts() => _contacts;
+
+        public static Contact? GetContactById (int contactId) => _contacts.FirstOrDefault(x => x.ContactId == contactId);
+    }
+}
--- Contacts.Maui/Views/ContactPage.xaml.cs
+++ Contacts.Maui/Views/ContactPage.xaml.cs
@@ -1,3 +1,6 @@
+using Contacts.Maui.Models;
+using Contact = Contacts.Maui.Models.Contact;
+
 namespace Contacts.Maui.Views;
 
 public partial class ContactPage : ContentPage
@@ -5,15 +8,32 @@ public partial class ContactPage : ContentPage
 	public ContactPage()
 	{
 		InitializeComponent();
+
+		List<Contact> contacts = ContactRepository.GetContacts();
+
+		listContacts.ItemsSource = contacts;
 	}
 
-    private void btnEditContact_Clicked(object sender, EventArgs e)
+
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-		Shell.Current.GoToAsync(nameof(EditContactPage));
+        if (listContacts.SelectedItem != null)
+            await Shell.Current.GoToAsync($"{nameof(EditContactPage)}?Id={((Contact)listContacts.SelectedItem).ContactId}");
     }
 
-    private void btnAddContact_Clicked(object sender, EventArgs e)
+    private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
     {
-        Shell.Current.GoToAsync(nameof(AddContactPage));
+        listContacts.SelectedItem = null;
     }
+
+
+    //  private void btnEditContact_Clicked(object sender, EventArgs e)
+    //  {
+    //Shell.Current.GoToAsync(nameof(EditContactPage));
+    //  }
+
+    //  private void btnAddContact_Clicked(object sender, EventArgs e)
+    //  {
+    //      Shell.Current.GoToAsync(nameof(AddContactPage));
+    //  }
 }
